d6ac2b4e5098bd6bc4867e90b0642650ae5fe0e2
Oleg Galaburda <burdiuz@gmail.com>
2020-03-03 23:10:30 +0200

    fix sound issue

--- source/index.js
+++ source/index.js
@@ -1,17 +1,17 @@
-import AssetCanvas from './assetFactory';
-import Scene from './scene';
-import SparksHistory from './sparks';
-import SoundEffect from './soundEffect';
-import SoundEvents from './soundEvent';
+import AssetCanvas from "./assetFactory";
+import Scene from "./scene";
+import SparksHistory from "./sparks";
+import SoundEffect from "./soundEffect";
+import SoundEvents from "./soundEvent";
 
 let animatedImg, staticImg;
-const sound = new SoundEffect('sound', 5, 2);
+const sound = new SoundEffect("sound", 5, 2);
 const scene = new Scene(null, 24);
 const bgImg = new Image();
-const events = new SoundEvents('sound');
+const events = new SoundEvents("sound");
 
 const _animatedText = (cnx, x, y, angle) => {
-  cnx.fillStyle = '#F433FF';
+  cnx.fillStyle = "#F433FF";
   cnx.rotate(angle);
   cnx.font = '220px "AANeon"';
   cnx.fillText("C", x, y);
@@ -19,7 +19,7 @@ const _animatedText = (cnx, x, y, angle) => {
 };
 
 const _staticText = (cnx, x, y, angle) => {
-  cnx.fillStyle = '#F433FF';
+  cnx.fillStyle = "#F433FF";
   cnx.font = '92px "AANeon"';
   cnx.rotate(angle);
   cnx.fillText("Я_ВСЕГДА_ХОТЕЛ_БЫТЬ", x + 60, y);
@@ -28,57 +28,79 @@ const _staticText = (cnx, x, y, angle) => {
   cnx.rotate(-angle);
 };
 
-bgImg.src = 'data/background.jpg';
+bgImg.src = "data/background.jpg";
 
 let sparks;
 
 const start = () => {
-  sparks = new SparksHistory(canvas.getContext('2d'));
+  sparks = new SparksHistory(canvas.getContext("2d"));
   sound.play(true);
   events.add(1891, () => sparks.start(305, 667));
   events.add(3015, () => sparks.start(457, 643));
-}
+};
 
-bgImg.addEventListener('load', () => {
+const draw = () => {
   let assetFactory;
-  assetFactory = new AssetCanvas('asset_factory', bgImg, (context) => {
-    _animatedText(context, 182, 720, -(Math.PI / 180) * 10);
+
+  assetFactory = new AssetCanvas("asset_factory", bgImg, (context) => {
+    _animatedText(context, 182, 720, -Math.PI / 18);
   });
+
   assetFactory.compose().then((image) => {
     animatedImg = image;
+
     assetFactory.setTextDrawer((context) => {
-      _staticText(context, 120, 500, -(Math.PI / 180) * 10);
+      _staticText(context, 120, 500, -Math.PI / 18);
     });
+
     assetFactory.compose().then((image) => {
       staticImg = image;
       start();
     });
   });
+}
+
+bgImg.addEventListener("load", () => {
+  document.querySelector("#play_container").style.visibility = "visible";
 });
 
 scene.onFrame = (() => {
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
 
   return () => {
     if (!sound.playing) return;
     let ratio = sound.currentRatio;
     context.clearRect(0, 0, canvas.width, canvas.height);
     // use source-in to make lightened wall
-    context.filter = 'opacity(' + parseInt(75 + (1 - ratio) * 25 >>> 0) + '%)';
+    context.filter =
+      "opacity(" + parseInt((75 + (1 - ratio) * 25) >>> 0) + "%)";
     context.drawImage(staticImg, 0, 0);
-    context.filter = 'opacity(' + parseInt(ratio * 100 >>> 0) + '%)';
+    context.filter = "opacity(" + parseInt((ratio * 100) >>> 0) + "%)";
     context.drawImage(animatedImg, 0, 0);
-    context.filter = 'none';
+    context.filter = "none";
     if (sparks.animate()) {
       scene.invalid();
     }
   };
 })();
+
 sound.onChange = () => {
   scene.invalid();
 };
 
-document.addEventListener('visibilitychange', () => {
+let hasStarted = false;
+
+document.addEventListener("visibilitychange", () => {
+  if (!hasStarted) {
+    return;
+  }
+
   document.hidden ? sound.pause() : sound.play();
 });
+
+window.startAnimation = () => {
+  draw();
+  hasStarted = true;
+  document.querySelector("#play_container").remove();
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+
 module.exports = {
-  entry: ['babel-polyfill', './source/index.js'],
+  entry: ["babel-polyfill", "./source/index.js"],
   output: {
-    path: './',
-    filename: 'bin/app.bundle.js',
+    path: "./",
+    filename: "bin/app.bundle.js"
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      include: [
-        path.resolve(__dirname, 'node_modules/Color'),
-        path.resolve(__dirname, 'node_modules/Pixels'),
-        path.resolve(__dirname, 'node_modules/ConvexText'),
-        path.resolve(__dirname, 'node_modules/MultiHandlerFactory'),
-        path.resolve(__dirname, 'source')
-      ],
-      loader: 'babel-loader'
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "node_modules/Color"),
+          path.resolve(__dirname, "node_modules/Pixels"),
+          path.resolve(__dirname, "node_modules/ConvexText"),
+          path.resolve(__dirname, "node_modules/MultiHandlerFactory"),
+          path.resolve(__dirname, "source")
+        ],
+        loader: "babel-loader"
+      }
+    ]
   },
   plugins: [
     //*
-     new webpack.optimize.UglifyJsPlugin({
+    new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       }
-     })
-     //*/
+    })
+    //*/
   ],
   devServer: {
     inline: true
